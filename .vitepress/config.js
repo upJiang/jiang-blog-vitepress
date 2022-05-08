0e962a6912dc672281c8fd5c89f8e86ff623052c
530274949@qq.com <530274949@qq.com>
2022-05-08 22:43:00 +0800

    feat: 更新面试之道

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -297,7 +297,9 @@ async function getConfig() {
               { text: 'js基础', link: '/docs/howReview/jsBase' },
               { text: 'es6', link: '/docs/howReview/es6' },
               { text: '异步编程', link: '/docs/howReview/async' },
-              { text: '浏览器知识', link: '/docs/howReview/brower' }
+              { text: '浏览器知识', link: '/docs/howReview/brower' },
+              { text: '安全防范知识', link: '/docs/howReview/secure' },
+              { text: '性能优化', link: '/docs/howReview/performance' }
             ]
           }
         ],
